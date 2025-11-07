558cb7485bbe66d6e9e2c070dfc87c80831a6162
shenjunjian <40288193@qq.com>
2025-11-07 16:05:19 +0800

    2 添加vitepress

---
+++ blog/.vitepress/config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Vue Vapor Runtime 函数研究",
+  description: "vapor 编译模板后的运行时探秘",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "介绍", link: "/intro" },
+    ],
+
+    sidebar: [
+      {
+        text: "模板编译",
+        items: [
+          { text: "模板绑定", link: "/binds" },
+          { text: "事件绑定", link: "/events" },
+          { text: "组件", link: "/components" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/shenjunjian/vue-vapor-runtime-helper-study",
+      },
+    ],
+  },
+
+  lastUpdated: true,
+});
---
+++ blog/index.md
@@ -0,0 +1,19 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Vue Vapor Runtime 函数研究"
+  text: "vapor 编译模板后的运行时探秘"
+  actions:
+    - theme: brand
+      text: 介绍起因
+      link: /intro
+    # - theme: alt
+    #   text: API Examples
+    #   link: /api-examples
+
+features:
+  - title: 探究原理，执果索因
+    details: 根据vapor的编译结果，分析vapor 运行时原因
+---
